7aaa904c98990b51ddf896252aa27f8e3a94c75f
Sebastian Willert <willert@gmail.com>
2014-03-13 02:34:58 +0100

    Build env

---
+++ README.md
@@ -0,0 +1,3 @@
+# AUTHORS
+
+Sebastian Willert <s.willert@wecare.de>
---
+++ lib/Time/Naive.pm
@@ -0,0 +1,13 @@
+package Time::Naive;
+# ABSTRACT: naive handling of floating daytimes
+
+our $VERSION = '0.02';
+"This file is here to keep Minilla and File::Share happy";
+
+__END__
+
+=head1 AUTHORS
+
+Sebastian Willert <s.willert@wecare.de>
+
+=cut
